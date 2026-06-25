08e5b4efa552f89f9764f6c0aca18a449324e32a
ylro-gabriel <ylrgbrlmllr@gmail.com>
2026-06-25 17:31:24 +0800

    add game control buttons and styles for interaction

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function btnLeft() {
   document.getElementById("btn-left");
   console.log("Left");
 }
+
+function btnSelect() {
+  document.getElementById("btn-select");
+  console.log("Select");
+}
+
+function btnStart() {
+  document.getElementById("btn-start");
+  console.log("Start");
+}
+
+function btnKick() {
+  document.getElementById("btn-kick");
+  console.log("Kick");
+}
+
+function btnPunch() {
+  document.getElementById("btn-punch");
+  console.log("Punch");
+}
+
+function btnJump() {
+  document.getElementById("btn-jump");
+  console.log("Jump");
+}
