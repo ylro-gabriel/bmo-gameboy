9bfcf132d232edd8939b75df82e77ac90a0460cc
ylro-gabriel <ylrgbrlmllr@gmail.com>
2026-06-25 15:24:58 +0800

    add HTML structure and JavaScript functions for game controls

---
+++ script.js
@@ -0,0 +1,19 @@
+function btnUp() {
+  document.getElementById("btn-up");
+  console.log("Up");
+}
+
+function btnDown() {
+  document.getElementById("btn-down");
+  console.log("Down");
+}
+
+function btnRight() {
+  document.getElementById("btn-right");
+  console.log("Right");
+}
+
+function btnLeft() {
+  document.getElementById("btn-left");
+  console.log("Left");
+}
